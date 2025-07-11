41535de5da86aabae73e621cb89a1e0e63223542
Matías Albites <matiasalbites1512@gmail.com>
2025-07-11 12:26:22 -0500

    feat: agregar código base frontend

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+
+import Button from '../../components/button/button';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a LinkTree Clone</h1>
+      <Button text="Haz clic aquí" />
+    </div>
+  );
+};
+
+export default Home;
