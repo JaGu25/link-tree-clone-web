8791a336b17741e7f317d6642a5bd61ad9e7f9d9
MatiasAlbites2004 <matiasalbites1512@gmail.com>
2025-08-31 20:25:16 -0500

    Feat Dashboard Final..

--- src/components/AdminNavbar/AdminNavbar.jsx
+++ src/components/AdminNavbar/AdminNavbar.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import styles from "./AdminNavbar.module.css";
 import { FaUserCircle } from "react-icons/fa";
+import { NavLink, useLocation } from "react-router-dom";
 
 const AdminNavbar = () => {
+  const location = useLocation();
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>LinktreePro Admin</div>
+      <div className={styles.leftSection}>
+        <div className={styles.logo}>LinktreePro Admin</div>
+        
+        {location.pathname === "/config" && (
+          <NavLink 
+            to="/dashboard" 
+            className={styles.logoLink}
+          >
+            Dashboard
+          </NavLink>
+        )}
+
+        {location.pathname === "/dashboard" && (
+          <NavLink 
+            to="/config" 
+            className={styles.logoLink}
+          >
+            Configuración
+          </NavLink>
+        )}
+      </div>
 
       <div className={styles.rightSection}>
         <FaUserCircle className={styles.icon} />
